6108340703064c34f44d8d49cfe2056e5ad0e3d3
marcricahuerta <marckenneth100@gmail.com>
2021-12-19 13:02:45 +0800

    Test Commit

---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,175 @@
+<!DOCTYPE html>
+
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+  
+    <!-- CSRF Token -->
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+
+    <title>{{ config('app.name', 'Laravel') }}</title>
+
+
+    <!-- Google Font: Source Sans Pro -->
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700&display=fallback">
+    <!-- Font Awesome Icons -->
+
+    <link rel="stylesheet" href="{{URL::asset('dist/css/adminlte.min.css')}}">
+    <link rel="stylesheet" href="{{URL::asset('plugins/fontawesome-free/css/all.min.css')}}">
+
+<style>
+    .sidebar-light-pink .nav-sidebar > .nav-item > .nav-link.active {
+    background-color: #e83e8c;
+    color: #fff;
+    }
+</style>
+</head>
+
+<body class="hold-transition sidebar-mini">
+    <!-- Wrapper Start -->
+    <div class="wrapper">
+
+        <!-- Navbar Start -->
+        <nav class="main-header navbar navbar-expand navbar-white navbar-light">
+            <!-- Left navbar Start -->
+            <ul class="navbar-nav">
+            <li class="nav-item">
+                <a class="nav-link" data-widget="pushmenu" href="#" role="button"><i class="fas fa-bars"></i></a>
+            </li>
+            </ul>
+            <!-- Left navbar End -->
+
+            <!-- Right navbar Start -->
+            <ul class="navbar-nav ml-auto"> 
+                @guest
+                    @if (Route::has('login'))
+                        <li class="nav-item">
+                            <a class="nav-link" href="{{ route('login') }}">{{ __('Login') }}</a>
+                        </li>
+                    @endif
+
+                    @if (Route::has('register'))
+                        <li class="nav-item">
+                            <a class="nav-link" href="{{ route('register') }}">{{ __('Register') }}</a>
+                        </li>
+                    @endif
+                @else
+                    <li class="nav-item dropdown">
+                        <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
+                            {{ Auth::user()->name }}
+                        </a>
+
+                        <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
+                            <a class="dropdown-item" href="{{ route('logout') }}"
+                                onclick="event.preventDefault();
+                                                document.getElementById('logout-form').submit();">
+                                {{ __('Logout') }}
+                            </a>
+
+                            <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
+                                @csrf
+                            </form>
+                        </div>
+                    </li>
+                @endguest
+            </ul>
+            <!-- Right navbar End -->
+        </nav>
+        <!-- navbar End -->
+
+        <!-- Main Sidebar Container -->
+        <aside class="main-sidebar sidebar-dark-primary elevation-4">
+
+            <!-- Sidebar Start -->
+            <div class="sidebar">
+                
+                <!-- Sidebar Top -->
+                <div class="user-panel mt-3 pb-3 mb-3 d-flex">
+                    <div class="image">
+                        <img src="/img/apc_logo.png" class="img-circle elevation-2" alt="User Image">
+                    </div>
+                    <div class="info">
+                        <a href="#" class="d-block">SAO-OECS</a>
+                    </div>
+                </div>
+
+                <!-- Sidebar Menu Start -->
+                <nav class="mt-2">
+                    <ul class="nav nav-pills nav-sidebar flex-column" data-widget="treeview" role="menu" data-accordion="false">
+                    <!-- Add icons to the links using the .nav-icon class
+                        with font-awesome or any other icon font library -->
+
+                        @include('layouts.menu')
+
+                        <!-- Dashboard -->
+                        <li class="nav-item">
+                            <a href="#" class="nav-link">
+                                <i class="nav-icon fas fa-th"></i>
+                                <p>DASHBOARD</p>
+                            </a>
+                        </li>
+
+                        <!-- Submitted Forms -->
+                        <li class="nav-item">
+                            <a href="#" class="nav-link">
+                                <i class="nav-icon fas fa-file-download"></i>
+                                <p>SUBMITTED FORMS<span class="right badge badge-danger">New</span></p>
+                            </a>
+                        </li>
+
+                        <!-- Records -->
+                        <li class="nav-item">
+                            <a href="#" class="nav-link">
+                                <i class="nav-icon fas fa-folder"></i>
+                                <p>RECORDS</p>
+                            </a>
+                        </li>
+
+                         <!-- Applicants -->
+                         <li class="nav-item">
+                            <a href="#" class="nav-link">
+                            <i class="nav-icon fas fa-user-clock"></i>
+                                <p>APPLICANTS<span class="right badge badge-danger">New</span></p>
+                            </a>
+                        </li>
+                    </ul>
+                </nav>
+                <!-- Sidebar Menu End -->
+
+            </div>
+            <!-- Sidebar End -->
+        
+        </aside>
+
+        <!-- Page Content Start -->
+        <div class="content-wrapper">
+
+            <!-- Content Header Start -->
+            <div class="content-header">
+                <div class="container-fluid">
+                    <div class="col-sm-12">
+
+                        @yield('content')
+
+                    </div>
+                </div>
+            </div>
+            <!-- content-header End -->
+
+        </div>
+        <!-- Page Content End -->
+
+        <footer class="main-footer d-flex justify-content-sm-center">
+            <strong>Copyright &copy; 2021 <a href="https://www.facebook.com">Brewing Minds</a>.</strong> All rights reserved.
+        </footer>
+    </div>
+<!-- Wrapper End -->
+
+<!-- REQUIRED SCRIPTS -->
+    <script src="{{ asset('js/app.js') }}"></script>
+    <script type="text/javascript" src="{{ URL::asset('plugins/jquery/jquery.min.js') }} "></script>
+    <script type="text/javascript" src="{{ URL::asset('plugins/bootstrap/js/bootstrap.bundle.min.js') }} "></script>
+    <script type="text/javascript" src="{{ URL::asset('dist/js/adminlte.min.js') }} "></script>
+</body>
+</html>
---
+++ resources/views/layouts/menu.blade.php
@@ -0,0 +1,33 @@
+<!-- Use this as reference for forms dropdown -->
+
+<!-- Change the icons accordingly -->
+
+<!-- Parent -->
+<li class="nav-item">
+    <a href="#" class="nav-link">
+        <i class="nav-icon fas fa-tachometer-alt"></i>
+        <p>Test<i class="right fas fa-angle-left"></i></p>
+    </a>
+    
+    <!-- Children -->
+    <ul class="nav nav-treeview">
+
+        <!-- Test 1 -->
+        <li class="nav-item">
+            <a href="#" class="nav-link">
+                <i class="far fa-circle nav-icon"></i>
+                <p>Test 1</p>
+            </a>
+        </li>
+
+        <!-- Test 2 -->
+        <li class="nav-item">
+            <a href="#" class="nav-link">
+                <i class="far fa-circle nav-icon"></i>
+                <p>Test 2</p>
+            </a>
+        </li>
+    </ul>
+</li>
+
+<!-- Use this as reference for forms dropdown -->
